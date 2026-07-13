dba9e9614eb3f8f0ad59cf3d452d42ad7b2c4b0b
Ameesha <ameeshaa2003@gmail.com>
2026-07-13 10:33:38 +0530

    commit:compete week4-day1-task5

---
+++ week4/day1/task5/counter.js
@@ -0,0 +1,30 @@
+boxes=document.querySelectorAll('.box');
+function animateCount(element){
+    let start=null;
+    const duration=2000;
+    const target=parseInt(element.getAttribute('data-target'),10);
+    function step(timestamp){
+        if(!start)
+            start=timestamp;
+        const progress=timestamp-start
+        const progressPersentage=Math.min(progress/duration,1);
+        const currentValue=Math.floor(progressPersentage*target)
+        element.textContent=currentValue.toLocaleString();
+        if (progress<duration){
+            requestAnimationFrame(step)
+        }
+        else{
+            element.textContent=target.toLocaleString();
+        }
+    }
+requestAnimationFrame(step);
+}
+counterObserver=new IntersectionObserver((elements,observer)=>{
+    elements.forEach(el=>{
+        if(el.isIntersecting){
+            animateCount(el.target);
+            observer.unobserve(el.target)
+        }
+    });
+},{threshold:1});
+boxes.forEach((e)=>counterObserver.observe(e));
