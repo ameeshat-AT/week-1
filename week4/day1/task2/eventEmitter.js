d2059478d3bc45cbabc7df59924bd44059497716
Ameesha <ameeshaa2003@gmail.com>
2026-07-14 18:02:23 +0530

    feat:Complete day1

--- week4/day1/task2/eventEmitter.js
+++ week4/day1/task2/eventEmitter.js
@@ -1,77 +1,76 @@
-class EventEmitter{
-    constructor(){
-        this.events={};
+class EventEmitter {
+    constructor() {
+        this.events = {};
     }
-    on(event,listener){
-        if(!this.events[event])
-            this.events[event]=[];
+    on(event, listener) {
+        if (!this.events[event])
+            this.events[event] = [];
         this.events[event].push(listener);
         // return this;
     }
-    emit(event,...args){
-        if(this.events[event])
-             this.events[event].forEach(listener=>listener(...args));
-        if(event!=='*' && this.events['*']){
-            this.events['*'].forEach(listener=>listener(event,...args));
+    emit(event, ...args) {
+        if (this.events[event])
+            this.events[event].forEach(listener => listener(...args));
+        if (event !== '*' && this.events['*']) {
+            this.events['*'].forEach(listener => listener(event, ...args));
         }
         // return this;
     }
-    off(event,listener){
-        if(!this.events[event])
+    off(event, listener) {
+        if (!this.events[event])
             return;
-        this.events[event]= this.events[event].filter(l=>l!==listener);
+        this.events[event] = this.events[event].filter(l => l !== listener);
         // return this;
     }
-    once(event,listener){
-        const wrapper=(...args)=>
-        {
-            this.off(event,wrapper);
+    once(event, listener) {
+        const wrapper = (...args) => {
+            this.off(event, wrapper);
             listener(...args);
 
         };
-        this.on(event,wrapper);
+        this.on(event, wrapper);
         // return this;
     }
 }
-const emitter=new EventEmitter();
-class UserStore extends EventEmitter{
-    constructor(){
+const emitter = new EventEmitter();
+class UserStore extends EventEmitter {
+    constructor() {
         super();
-        this.users=[];
+        this.users = [];
     }
-    addUser(username){
+    addUser(username) {
         this.users.push(username);
-        this.emit('userAdded',username);
+        this.emit('userAdded', username);
     }
-    removeUser(username){
-        this.users=this.users.filter(u=>u!==username);
-        this.emit('userRemoved',username);
+    removeUser(username) {
+        this.users = this.users.filter(u => u !== username);
+        this.emit('userRemoved', username);
     }
-    updateUser(oldName,newName){
-        let index=this.users.indexOf(oldName);
-        if(index !==-1){
-            this.users[index]=newName;
-            this.emit('userUpdated',newName);
+    updateUser(oldName, newName) {
+        let index = this.users.indexOf(oldName);
+        if (index !== -1) {
+            this.users[index] = newName;
+            this.emit('userUpdated', newName);
         }
     }
 }
-const myUser=new UserStore();
-myUser.on('*',(eventName,data)=>{
+const myUser = new UserStore();
+myUser.on('*', (eventName, data) => {
     console.log(`Event ${eventName}  data=${data}`);
 })
-myUser.once('userAdded',(name)=>console.log(`${name}is our first user`));
+myUser.once('userAdded', (name) => console.log(`${name}is our first user`));
 myUser.addUser('geetha');
-myUser.updateUser('geetha','Alice');
+myUser.updateUser('geetha', 'Alice');
 myUser.removeUser('Alice');
-myUser.on('userAdded',(name)=>{
+myUser.on('userAdded', (name) => {
     console.log(`welcome ${name}`);
 })
-myUser.once('userAdded',(name)=>{
+myUser.once('userAdded', (name) => {
     console.log(`${name} is our first gust`);
 })
-myUser.on('userRemoved',(name)=>{
+myUser.on('userRemoved', (name) => {
     console.log(`thankyou ${name}`);
 })
-myUser.on('userUpdated',(name)=>{
+myUser.on('userUpdated', (name) => {
     console.log(`welcome ${name}`);
 })
